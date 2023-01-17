b95ff4b4a325b8aba29513f1a9c18314cec9a8ad
HyejinUm <uhj1993@naver.com>
2023-01-17 23:57:04 +0900

    🧱 CRA -> Vite 전환
    - "jest 추가"

--- src/pages/App.test.tsx
+++ src/pages/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from '.';
+import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/hello/i);
+  const linkElement = screen.getByText('Vite + React');
   expect(linkElement).toBeInTheDocument();
 });

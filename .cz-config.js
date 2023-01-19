69fb03b9e4abe0a4eeefc91ad6cda8b0316b29aa
HyejinUm <uhj1993@naver.com>
2023-01-19 23:33:41 +0900

    🐛 fix(custom): cz-config 수정 #4
    ISSUES CLOSED: #4

--- .cz-config.js
+++ .cz-config.js
@@ -1,20 +1,27 @@
 module.exports = {
   types: [
-    { name: 'feat \t New feature', value: ':sparkles: feat' },
-    { name: 'fix \t Bug fix', value: ':bug: fix' },
-    { name: 'test \t Add, update, or pass tests', value: ':white_check_mark: test' },
-    { name: 'refactor \t Refactor code', value: ':recycle: refactor' },
-    { name: 'wrench \t Add or update configuration', value: ':wrench: config' },
-    { name: 'style \t Add or update the UI and style files', value: ':lipstick: style' },
-    { name: 'docs \t Change Documentation', value: ':memo: docs' },
-    { name: 'perf \t Improves performance', value: ':zap: perf' },
-    { name: 'security \t Fix or prevent security issue', value: ':lock: security' },
-    { name: 'upgrade \t Upgrade dependency', value: ':arrow_up: upgrade' },
-    { name: 'plus \t New dependency', value: ':heavy_plus_sign: plus' },
-    { name: 'minus \t Remove dependency', value: ':heavy-minus-sign: minus' },
+    { name: 'feat \t\t✨ 기능추가', value: '✨ feat' },
+    { name: 'fix \t\t🐛 버그픽스', value: '🐛 fix' },
+    {
+      name: 'test \t\t✅ 테스트 코드 추가',
+      value: '✅ test',
+    },
+    {
+      name: 'refactor \t🔨 리팩터링',
+      value: '🔨 refactor',
+    },
+    {
+      name: 'style \t💄 스타일 추가 및 수정',
+      value: '💄 style',
+    },
+    { name: 'docs \t\t📝 문서 업데이트', value: '📝 docs' },
+    {
+      name: 'chore \t🔧 빌드 및 구조 수정',
+      value: '🔧 chore',
+    },
   ],
-  skipQuestions: ['body', 'footer'],
-  scopes: ['api', 'ui'],
-  allowCustomScopes: true,
+  allowCustomScopes: false,
+  allowBreakingChanges: ['feat', 'fix'],
+  skipQuestions: ['body'],
   subjectLimit: 100,
 };
